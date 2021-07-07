7a6ff44c056d48c32bdb6efd4ea5d49c5811d909
sreesindhusruthiyadavalli <sssindh@gmail.com>
2021-07-07 19:09:27 +0530

    Insert elements in ll

--- 1.linked-list.go
+++ 1.linked-list.go
@@ -34,30 +34,65 @@ func (ll *LinkedList) Len() int{
 
 func (ll *LinkedList) Print() {
 	if ll.head == nil{
-		fmt.Println("")
+		fmt.Print("NULL")
 		return
 	}
-	temp := ll.head
-	for temp != nil{
-		fmt.Println(temp.data)
-		temp = temp.next
+	for ll.head != nil {
+		fmt.Printf("%+v -->",ll.head.data)
+		ll.head = ll.head.next
+		ll.Print()
 	}
+}
+
+func (ll *LinkedList) Insert(data int, position int){
+	var currPosition int
+	currentNode := ll.head
+	temp := new(Node)
+	temp.data = data
+	//This inserts new element as linked list is empty
+	if ll.head == nil{
+		ll.head = temp
+		return
+	}
+	//This inserts at beginning of the linked list.
+	if position == 0{
+		temp.next = ll.head
+		ll.head = temp
+	}
+	for currPosition < position-1 && currentNode != nil {
+		currentNode = currentNode.next
+		currPosition ++
+		//fmt.Println(currentNode.data, currPosition)
+	}
+	//Update the links
+	existingNextNode := currentNode.next
+	currentNode.next = temp
+	currentNode = currentNode.next
+	currentNode.next = existingNextNode
 }
 
 func main(){
 	llist := new(LinkedList)
-	node1 := new(Node)
-	node1.NewNode(3, nil)
-	llist.head = node1
-	//fmt.Println(llist.Len())
-	node2 := new(Node)
-	node2.NewNode(7, nil)
-	llist.head.next = node2
-	//fmt.Println(llist.Len())
-	node3 := new(Node)
-	node3.NewNode(10, nil)
-	llist.head.next.next = node3
+	//node1 := new(Node)
+	//node1.NewNode(3, nil)
+	////llist.head = node1
+	////fmt.Println(llist.Len())
+	//node2 := new(Node)
+	//node2.NewNode(7, nil)
+	////llist.head.next = node2
+	////fmt.Println(llist.Len())
+	//node3 := new(Node)
+	//node3.NewNode(10, nil)
+	////llist.head.next.next = node3
+	////llist.Print()
+	llist.Insert(3, 0)
+	llist.Insert(7, 1)
+	llist.Insert(10, 2)
+	//llist.Print()
+	llist.Len()
+	llist.Insert(4, 2)
 	llist.Print()
+	llist.Len()
 }
 
 
@@ -65,12 +100,6 @@ func main(){
 
 
 /*
-output:
-&{1 <nil>}
-<nil>
-1
-&{1 0xc000010260}
-&{2 <nil>}
-<nil>
-2
+3 -->7 -->10 -->NULL
+
  */
